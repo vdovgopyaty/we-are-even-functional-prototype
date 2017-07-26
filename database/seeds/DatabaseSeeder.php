c46969ae3101f7e64df358d2b78397b7fe03b467
Vladislav Dovgopyaty <vdovgopyaty@gmail.com>
2017-07-26 17:32:44 +0300

    Fix seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
-        $this->call(CurrenciesTableSeeder::class);
+        $this->call('UsersTableSeeder');
+        $this->call('CurrenciesTableSeeder');
 //        $this->call(CountriesTableSeeder::class);
     }
 }
