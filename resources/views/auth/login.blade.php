b15e452cfad2b63ff6c1add2c93344f136fa129c
Vladislav Dovgopyaty <vdovgopyaty@gmail.com>
2017-07-31 18:15:24 +0300

    Login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,86 +1,56 @@
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+@extends('layouts.app')
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+@section('content')
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+<div class="mdl-grid">
+    <div class="mdl-cell mdl-cell--4-col mdl-cell--12-col-phone">
+        <form class="form-signin" method="POST" action="{{ route('login') }}">
+            {{ csrf_field() }}
 
-    <!-- Bootstrap -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://v4-alpha.getbootstrap.com/examples/signin/signin.css">
+            <div class="mdl-typography--display-2">
+                <h2>Please sign in</h2>
+            </div>
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
-<body>
-<div class="container">
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                <input class="mdl-textfield__input{{ $errors->has('email') ? ' has-error' : '' }}" type="email"
+                       id="email"
+                       name="email" value="{{ old('email') }}" required autofocus>
+                <label class="mdl-textfield__label" for="email">Email-адрес</label>
+            </div>
 
-    <form class="form-signin" method="POST" action="{{ route('login') }}">
-        {{ csrf_field() }}
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                <input class="mdl-textfield__input{{ $errors->has('password') ? ' has-error' : '' }}" type="password"
+                       id="password" name="password" required>
+                <label class="mdl-textfield__label" for="password">Пароль</label>
+            </div>
 
-        <h2 class="form-signin-heading">Please sign in</h2>
-
-        <label for="email" class="sr-only">Email address</label>
-        <input type="email" id="email" class="form-control{{ $errors->has('email') ? ' has-error' : '' }}" placeholder="Email address" name="email" value="{{ old('email') }}" required autofocus>
-
-        <label for="password" class="sr-only">Password</label>
-        <input type="password" id="password" class="form-control{{ $errors->has('password') ? ' has-error' : '' }}" placeholder="Password" name="password" required>
-
-        <div class="checkbox">
-            <label>
-                <input type="checkbox" value="remember-me" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember me
+            <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="checkbox">
+                <input type="checkbox" id="checkbox" class="mdl-checkbox__input" value="remember-me"
+                       name="remember" {{ old('remember') ? 'checked' : '' }}>
+                <span class="mdl-checkbox__label">Запомнить меня</span>
             </label>
-        </div>
 
-        <div class="form-group">
-            <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-        </div>
+            <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" type="submit">Войти</button>
 
-        <div class="form-group text-center">
-            <a class="btn btn-link" href="{{ route('register') }}">
-                Register
-            </a>
-            <a class="btn btn-link" href="{{ route('password.request') }}">
-                Forgot Your Password?
-            </a>
-        </div>
+            <p class="cta-links">
+                <a href="{{ route('register') }}">Зарегистрироваться</a>
+                <span class="divider">|</span>
+                <a href="{{ route('password.request') }}">Забыли пароль?</a>
+            </p>
 
-        @if ($errors->has('email'))
-        <span class="help-block">
-            <strong>{{ $errors->first('email') }}</strong>
-        </span>
-        @endif
+            @if ($errors->has('email'))
+            <span class="help-block">
+                <strong>{{ $errors->first('email') }}</strong>
+            </span>
+            @endif
 
-        @if ($errors->has('password'))
-        <span class="help-block">
-            <strong>{{ $errors->first('password') }}</strong>
-        </span>
-        @endif
-    </form>
-
+            @if ($errors->has('password'))
+            <span class="help-block">
+                <strong>{{ $errors->first('password') }}</strong>
+            </span>
+            @endif
+        </form>
+    </div>
 </div>
 
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-<!-- Scripts -->
-<script src="{{ asset('js/app.js') }}"></script>
-</body>
-</html>
-
-
-
-
-
+@endsection
